3a42116feba2af6393bb7480706035be9e0fa59d
koficodedat <koficodedat@gmail.com>
2017-06-19 19:59:35 -0400

    Added some common simple but tedious utilities functions

---
+++ el.ts
@@ -0,0 +1,26 @@
+import * as utility from './utility-exports'
+
+(function (root, factory) {
+
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        root.ajs = factory();
+    }
+
+}(this, function () {
+    return {
+        //utilities
+        arraySum:       utility.Utility._arraySum,
+        average:        utility.Utility._average,
+        compare:        utility.Utility._compare,
+        inversions:     utility.Utility._inversions,
+        isSorted:       utility.Utility._isSorted,
+        shuffle:        utility.Utility._knuthShuffle,
+        randSeq:        utility.Utility._randSeq,
+        swapInArray:    utility.Utility._swapInArray,
+        uniform:           utility.Utility._uniform
+    };
+}));
---
+++ src/helpers/meregSortHelper.ts
@@ -0,0 +1,60 @@
+import compare from '../util/compare';
+
+export function sort<T>(array: T[],aux: T[], lo: number, hi: number){
+
+    if( hi <= lo ) return;
+
+    let mid: number = Math.floor( lo + ( hi - lo ) / 2 );
+    sort( array, aux, lo, mid );
+    sort( array, aux, mid + 1, hi );
+    merge( array, aux, lo, mid, hi)
+}
+
+export function merge<T>(array: T[], aux: T[], lo: number, mid: number, hi: number){
+
+    if( array instanceof Array ) { //TODO: implement a function to check if all values of the list are of the same type
+
+        // copy to aux[]
+        for( let i = lo; i <= hi; i++ ) aux[i] = array[i];
+
+        // merge back to array[]
+        let i = lo;
+        let j = mid + 1;
+
+        for( let k = lo; k <= hi; k++ ){
+
+            if( i > mid ) array[k] = aux[j++];
+            else if( j > hi ) array[k] = aux[i++];
+            else if( compare( aux[j], aux[i] ) === -1 ) array[k] = aux[j++];
+            else array[k] = aux[i++];
+
+        }
+    }
+
+}
+
+export function getInversions<T>(array: T[], aux: T[], lo: number, mid: number, hi: number): number{
+
+    let inversions: number = 0;
+
+    if( array instanceof Array ) { //TODO: implement a function to check if all values of the list are of the same type
+
+        // copy to aux[]
+        for( let i = lo; i <= hi; i++ ) aux[i] = array[i];
+
+        // merge back to array[]
+        let i = lo;
+        let j = mid + 1;
+
+        for( let k = lo; k <= hi; k++ ){
+
+            if( i > mid ) array[k] = aux[j++];
+            else if( j > hi ) array[k] = aux[i++];
+            else if( compare( aux[j], aux[i] ) === -1 ){ array[k] = aux[j++]; inversions += (mid - i + 1); }
+            else array[k] = aux[i++];
+
+        }
+    }
+
+    return inversions;
+}
